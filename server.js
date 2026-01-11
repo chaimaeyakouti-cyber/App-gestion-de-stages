f2d263e778a448455babf8abb80aae1fe45c50c4
chaimaeyakouti-cyber <chaimaeyakouti@gmail.com>
2026-01-11 23:03:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-// 1. Appel des outils installés
+
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
-// 2. Initialisation de l'application
-const app = express();
-app.use(cors()); // Autorise ton site à communiquer avec ce serveur
-app.use(express.json()); // Permet de lire les données envoyées (ex: formulaires)
 
-// 3. Configuration de la connexion à MySQL
+const app = express();
+app.use(cors()); 
+app.use(express.json()); 
+
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -16,7 +15,7 @@ const db = mysql.createConnection({
     database: 'inpt_internship_hub'
 });
 
-// 4. Test de la connexion
+
 db.connect((err) => {
     if (err) {
         console.error('Erreur de connexion à MySQL :', err);
@@ -24,7 +23,7 @@ db.connect((err) => {
     }
     console.log('Bravo ! Node.js est connecté à ta base MySQL.');
 });
-// Route pour récupérer toutes les offres de stage
+
 app.get('/api/offers', (req, res) => {
     const sql = "SELECT * FROM offers";
     db.query(sql, (err, data) => {
@@ -51,15 +50,13 @@ app.post('/api/offers', (req, res) => {
 });
 
 app.post('/api/login', (req, res) => {
-    // On extrait l'id et le password
+  
     let { id, password } = req.body;
     
-    // ACTION : On force l'id à devenir un nombre entier pour MySQL
     const numericId = parseInt(id); 
 
     const sql = "SELECT id, full_name, role FROM users WHERE id = ? AND password = ?";
     
-    // On utilise numericId au lieu de id
     db.query(sql, [numericId, password], (err, results) => {
         if (err) {
             console.error("Erreur SQL :", err);
@@ -73,7 +70,7 @@ app.post('/api/login', (req, res) => {
         }
     });
 });
-// Route pour modifier une offre existante
+
 app.put('/api/offers/:id', (req, res) => {
     const { title, type, filiere, description } = req.body;
     const offerId = req.params.id;
@@ -85,12 +82,12 @@ app.put('/api/offers/:id', (req, res) => {
         res.json({ message: "Offre mise à jour !" });
     });
 });
-// Route ajustée pour ta table applications réelle
+
 app.post('/api/applications', (req, res) => {
-    // On extrait les données envoyées par le frontend
+    
     const { offer_id, student_id, cv_url, motivation_text } = req.body;
     
-    // Requête SQL utilisant tes colonnes exactes : motivation_text
+    
     const sql = "INSERT INTO applications (offer_id, student_id, cv_url, motivation_text, status) VALUES (?, ?, ?, ?, 'En attente')";
     
     db.query(sql, [offer_id, student_id, cv_url, motivation_text], (err, result) => {
@@ -123,10 +120,10 @@ app.get('/api/applications/company/:companyId', (req, res) => {
         res.json(results);
     });
 });
-// Route pour mettre à jour le statut d'une candidature
+
 app.put('/api/applications/:id/status', (req, res) => {
     const appId = req.params.id;
-    const { status } = req.body; // 'Acceptée' ou 'Refusée'
+    const { status } = req.body; 
 
     const sql = "UPDATE applications SET status = ? WHERE id = ?";
     
@@ -138,9 +135,9 @@ app.put('/api/applications/:id/status', (req, res) => {
         res.json({ success: true, message: `Statut mis à jour : ${status}` });
     });
 });
-// Route pour récupérer tous les étudiants (Admin)
+
 app.get('/api/admin/students', (req, res) => {
-    // On ne sélectionne que les utilisateurs ayant le rôle 'student'
+    
     const sql = "SELECT id, full_name, filiere FROM users WHERE role = 'student'";
     
     db.query(sql, (err, results) => {
@@ -191,7 +188,7 @@ app.delete('/api/offers/:id', (req, res) => {
     });
 });
 
-// 5. Lancement du serveur sur le port 3000
+
 app.listen(3000, () => {
     console.log('Serveur en attente sur http://localhost:3000');
 });
